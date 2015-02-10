04999f7e37534b9bec8ba1590768869eabe8f9a3
David Long <longda@gmail.com>
2015-02-10 14:40:00 -0800

    trying out routing

--- jukebox/js/controllers.js
+++ jukebox/js/controllers.js
@@ -6,10 +6,12 @@ var jukeboxControllers = angular.module('jukeboxControllers', []);
 
 jukeboxControllers.controller('PlaylistCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
-    console.log('IM IN YR playlist controller...')
+    console.log('IM IN YR playlist controller...');
+    $scope.username = $routeParams.username;
+    $scope.setname = $routeParams.setname;
 }]);
 
 jukeboxControllers.controller('PizzaCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
-    console.log('IM IN YR pizza controller...')
+    console.log('IM IN YR pizza controller...');
 }]);
