93c0c79df86c6b3e0de34ac6e3b73b32abb48158
David Long <longda@gmail.com>
2015-03-21 17:59:10 -0700

    lime

--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,12 @@ gulp.task('minify-css', function(){
     .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
     .pipe(rename('green.min.css'))
     .pipe(gulp.dest('./css/'));
+  gulp.src('./css/lime.css') // set this to the file(s) you want to minify. 
+    .pipe(minifyCSS())
+    .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
+    .pipe(rename('lime.min.css'))
+    .pipe(gulp.dest('./css/'));
   gulp.src('./css/purple.css') // set this to the file(s) you want to minify. 
     .pipe(minifyCSS())
     .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
@@ -172,6 +178,17 @@ gulp.task('pre-process', function(){
         .pipe(browserSync.reload({stream:true}));
       }));
   gulp.src('./_sass/green.scss')
+    .pipe(watch(function(files) {
+      return files.pipe(sass()).on('error', errorHandler)
+        .pipe(size({gzip: false, showFiles: true, title:'without vendor prefixes'}))
+        .pipe(size({gzip: true, showFiles: true, title:'without vendor prefixes'}))
+        .pipe(prefix())
+        .pipe(size({gzip: false, showFiles: true, title:'after vendor prefixes'}))
+        .pipe(size({gzip: true, showFiles: true, title:'after vendor prefixes'}))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({stream:true}));
+      }));
+  gulp.src('./_sass/lime.scss')
     .pipe(watch(function(files) {
       return files.pipe(sass()).on('error', errorHandler)
         .pipe(size({gzip: false, showFiles: true, title:'without vendor prefixes'}))
