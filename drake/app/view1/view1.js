f9b4b42617b79b4c64cf6c33c06c294eb32207fb
David Long <longda@gmail.com>
2015-03-23 15:16:06 -0700

    drake

--- drake/app/view1/view1.js
+++ drake/app/view1/view1.js
@@ -9,6 +9,7 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', [function($scope) {
+  $scope.height = 200;
+  $scope.width = 300;
 }]);
