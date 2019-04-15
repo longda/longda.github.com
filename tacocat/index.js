48b08e9b97feb2b9ff90cdece017268733eb5050
David Long <david.long@metova.com>
2019-04-14 17:03:24 -0700

    tacocat

--- tacocat/index.js
+++ tacocat/index.js
@@ -34,10 +34,10 @@ function init() {
   controls.enableZoom = false;
   controls.enableDamping = true;
   controls.dampingFactor = 0.1;
-  controls.maxAzimuthAngle = 0.5;
-  controls.maxPolarAngle = 0.5;
-  controls.minAzimuthAngle = 0.5;
-  controls.minPolarAngle = 0.5;
+  // controls.maxAzimuthAngle = 0.5;
+  // controls.maxPolarAngle = 0.5;
+  // controls.minAzimuthAngle = 0.5;
+  // controls.minPolarAngle = 0.5;
 
 
   // scenes
